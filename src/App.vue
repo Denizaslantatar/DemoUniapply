705974bb402457dc72afbf4844d51953dcfa80a2
Muhammed ARSLAN <46113819+apsisx@users.noreply.github.com>
2022-11-10 13:52:39 +0300

    Visitor token işlemi gercekleşti

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isTokenLoaded: false,
+    };
+  },
+  mounted() {
+    let token = localStorage.getItem("token");
+    //Token yoksa bana token al,
+    if (!token) {
+      this.$store.dispatch("Visitor/GetVisitorToken").then(() => {
+        this.isTokenLoaded = true;
+      });
+    } else {
+      // token varsa zaten birsey yapmaya gerek yok
+      this.isTokenLoaded = true;
+    }
+  },
+};
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-if="isTokenLoaded" />
 </template>
--- src/Store/index.js
+++ src/Store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 
 import { ALoginMod } from "./Agency/Login.module";
 import { SLoginMod } from "./Student/Login.module";
+import { Visitor } from "./Public/visitor.module";
 
 const store = createStore({
   modules: {
     ALoginMod,
     SLoginMod,
+    Visitor,
   },
 });
 export default store;
