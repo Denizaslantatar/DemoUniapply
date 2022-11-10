1f7bdb8a504f88f4458cc5fb5e7418435ceed820
Denizas <100479163+Denizaslantatar@users.noreply.github.com>
2022-11-10 17:36:43 +0300

    .

--- src/Store/Student/Login.module.js
+++ src/Store/Student/Login.module.js
@@ -12,8 +12,8 @@ export const SLoginMod = {
       await SLoginService.Login(payload).then((response) => {
         if (response?.data?.token && response?.data?.success) {
           toast.success("You have logged in successfully");
-          localStorage.setItem("sToken", response.data.token);
-          localStorage.removeItem("vToken");
+          localStorage.setItem("token", response.data.token);
+          localStorage.removeItem("token");
           router.push("/student/dashboard");
         } else {
           toast.error("Incorrect email or password!");
