531282bee4354cd6daa9adafe2652ecbe6a2124b
Denizas <100479163+Denizaslantatar@users.noreply.github.com>
2022-11-14 11:36:17 +0300

    import işlemi

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,12 +3,14 @@ import { createStore } from "vuex";
 import { ALoginMod } from "./Agency/Login.module";
 import { SLoginMod } from "./Student/Login.module";
 import { Visitor } from "./Public/visitor.module";
+import { ALoginMod } from "./Agency/Login.module";
 
 const store = createStore({
   modules: {
     ALoginMod,
     SLoginMod,
     Visitor,
+    ARegister,
   },
 });
 export default store;
