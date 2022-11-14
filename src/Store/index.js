1b80ad797eb513b973c97872be3627a96dbee9ab
Denizas <100479163+Denizaslantatar@users.noreply.github.com>
2022-11-14 11:37:19 +0300

    .

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,7 +3,7 @@ import { createStore } from "vuex";
 import { ALoginMod } from "./Agency/Login.module";
 import { SLoginMod } from "./Student/Login.module";
 import { Visitor } from "./Public/visitor.module";
-import { ALoginMod } from "./Agency/Login.module";
+import { ARegisterMod } from "./Agency/Login.module";
 
 const store = createStore({
   modules: {
